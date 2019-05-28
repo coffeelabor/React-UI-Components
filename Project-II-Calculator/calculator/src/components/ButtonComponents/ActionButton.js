6c33a5992f8667e3ded9eb5c0b510eeb00ea5f7e
Reed James <ReedTJames1234@gmail.com>
2019-05-28 17:31:37 -0500

    WIP: JSX seems done, working on css

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,21 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+const Actions = [
+  { action: "#" },
+  { action: "##" },
+  { action: "###" },
+  { action: "####" }
+];
+
+const ActionButton = () => {
+  return (
+    <div className="Button">
+      {Actions.map(symbol => {
+        return <div className="box">{symbol.action}</div>;
+      })}
+    </div>
+  );
+};
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,27 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+const Numbers = [
+  { number: 1 },
+  { number: 2 },
+  { number: 3 },
+  { number: 4 },
+  { number: 5 },
+  { number: 6 },
+  { number: 7 },
+  { number: 8 },
+  { number: 9 },
+  { number: 0 }
+];
+
+const NumberButton = () => {
+  return (
+    <div className="NumberButton">
+      {Numbers.map(num => {
+        return <div className="box">{num.number}</div>;
+      })}
+    </div>
+  );
+};
+
+export default NumberButton;
